6867e96806c57222a62eff317dca939557882795
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-19 17:52:24 +0700

    mengecheck apakah node yang di maksud ada didalam list atau tidak

--- linkedlistoop/linkedlistoop.cpp
+++ linkedlistoop/linkedlistoop.cpp
@@ -80,3 +80,14 @@ bool List::delNode(int nim)/*Menghapus node dalam list*/ {
 	return true;
 }
 
+/*Chack apakah node yang dimaksud ada didalam list atau tidak*/
+bool List::Search(int nim, Node** previous, Node** current) {
+	*previous = START;
+	*current = START;
+	while ((*current != NULL) && (nim != (*current)->noMhs)) {
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
+
